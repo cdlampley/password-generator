2e9349da4b768e2b32dbd4acfb94b5a29c79d24b
Cherie Lampley <cherielampley@Cheries-MacBook-Pro.local>
2022-09-17 18:28:42 -0400

    added refresh page button

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#
 
 //declare variables for chosen elements
 const form = document.getElementById('form-button')
-
-//matching range and number inputs
 const range = document.getElementById('amountrange')
 const num = document.getElementById('amountnum')
+const btn = document.getElementById('btn')
+const updatedPassword = document.getElementById('updated-password')
+const getNewPassword = document.getElementById('refresh-button')
 
+// setup character length
 range.addEventListener('input', addRangeNum)
 num.addEventListener('input', addRangeNum)
 
@@ -16,23 +18,49 @@ function addRangeNum(e) {
     range.value = value
     num.value = value
 }
+// hide refresh page button
+getNewPassword.hidden = true
 
 //stop form from refreshing
 form.addEventListener('submit', e => {
     e.preventDefault()
 })
 
-
-//code for generating password
-const btn = document.getElementById('btn')
-const updatedPassword = document.getElementById('updated-password')
+// once button pressed
+const generatePassword = (length) => {
+    let result = ''
+    const alphaLength = alpha.length
+    for (let i = 0; i < length; i++) {
+        result += alpha.charAt(Math.floor(Math.random() * alphaLength))
+    }
+    return result
+}
+btn.addEventListener('click', generatePassword)
 
 newAlpha = () => {
-    const changeAlpha = "New Password"
+    const changeAlpha = generatePassword(5)
     updatedPassword.innerText = changeAlpha
 }
-
 btn.addEventListener('click', newAlpha)
+
+
+buttonUpdate = () => {
+    const buttonUpdateText = "Password Generated"
+    btn.innerText = buttonUpdateText
+}
+btn.addEventListener('click', buttonUpdate)
+
+btn.addEventListener('click', () => {
+    getNewPassword.hidden = false
+})
+
+
+//refresh page
+pageRefresh = () => {
+    window.location.reload()
+}
+getNewPassword.addEventListener('click', pageRefresh)
+
 
 
 //iterate through the characters with a for loop
